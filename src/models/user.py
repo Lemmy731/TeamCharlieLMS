940337de41cdab67854696297cc748a8140a46f1
Lemmy731 <lemmy.osiki@decagon.dev>
2026-04-06 12:38:14 +0100

    update current login user

--- src/models/user.py
+++ src/models/user.py
@@ -4,11 +4,12 @@ from datetime import datetime
 class User(db.Model):
     __tablename__= "users"
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(120), nullable=False)
-    last_name = db.Column(db.String(120), nullable=False)
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.now)
+    provider = db.Column(db.String(50), default="local")
 
     enrollments = db.relationship("Enrollment", backref="user", lazy=True)
     roles = db.relationship(
--- src/services/authservice/auth.py
+++ src/services/authservice/auth.py
@@ -1,6 +1,8 @@
 from src.dataaccess.authdataaccess.auth import AuthData
+import requests
 from flask import jsonify
 from src.models.user import User
+from config.config import Config
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import (create_access_token,create_refresh_token,get_jwt_identity, set_access_cookies,set_refresh_cookies
 )
@@ -29,10 +31,62 @@ class AuthService:
             roles = [role.name for role in response.roles]
             access_token = create_access_token(identity=str(response.id), additional_claims={"role":roles})
             refresh_token = create_refresh_token(identity=str(response.id), additional_claims={"role":roles})
-            resp = jsonify({'login': True})
+            resp = jsonify({"login": True,}
+                           )
             set_access_cookies(resp, access_token)
             set_refresh_cookies(resp, refresh_token)
             return resp, 200
+        return "invalid credential"
+        
+    def google_login(self):
+        google_provider_cfg = requests.get(Config.GOOGLE_DISCOVERY_URL).json()
+        authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+        request_uri = (
+        authorization_endpoint
+        + "?response_type=code"
+        + "&client_id=" + Config.GOOGLE_CLIENT_ID
+        + "&redirect_uri=" + Config.GOOGLE_REDIRECT_URI
+        + "&scope=openid email profile"
+    )
+        return request_uri
+
+    def google_callback(self, code):
+        google_provider_cfg = requests.get(Config.GOOGLE_DISCOVERY_URL).json()
+        token_endpoint = google_provider_cfg["token_endpoint"]
+        token_response = requests.post(
+            token_endpoint,
+            data={
+            "code": code,
+            "client_id": Config.GOOGLE_CLIENT_ID,
+            "client_secret": Config.GOOGLE_CLIENT_SECRET,
+            "redirect_uri": Config.GOOGLE_REDIRECT_URI,
+            "grant_type": "authorization_code",
+            },
+            )
+        token_json = token_response.json()
+        if "access_token" not in token_json:
+            return jsonify({"error": "Failed to get access token"}), 400
+        access_token = token_json.get("access_token")
+        userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+        userinfo_response = requests.get(userinfo_endpoint,headers={"Authorization": f"Bearer{access_token}"},)
+        userinfo = userinfo_response.json()
+        email = userinfo["email"]
+        name = userinfo.get("name")
+        response = self.auth_data.google_callback(email, name)
+        if not response:
+            return jsonify({"error": "User creation failed"}), 400
+        roles = [role.name for role in response.roles]
+        access_token = create_access_token(identity=str(response.id), additional_claims={"role":roles})
+        refresh_token = create_refresh_token(identity=str(response.id), additional_claims={"role":roles})
+        resp = jsonify({"login": True,
+                        "user": { 
+                            "id": response.id,
+                            "email": response.email, 
+                            "roles": roles
+                            }})
+        set_access_cookies(resp, access_token)
+        set_refresh_cookies(resp, refresh_token)
+        return resp
         
     def refresh(self):
         current_user = get_jwt_identity()
@@ -43,6 +97,9 @@ class AuthService:
     
     def get_current_user(self):
         user_id = get_jwt_identity()
+        print("JWT Identity:", get_jwt_identity())
+        if not user_id:
+            return jsonify({"error": "Invalid or missing token"}), 401
         response = self.auth_data.get_current_user(user_id)
         return response
       
